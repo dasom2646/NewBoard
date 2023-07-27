2222aabb197024f2a65f56b4961eaa9666a8cb67
dasom2646 <choi.dasom2646@gmail.com>
2023-07-27 17:31:36 +0900

    진행사항 보고

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,19 @@
 -- 맴버 테이블 생성
-CREATE TABLE IF NOT EXISTS member_tb
+CREATE TABLE IF NOT EXISTS TB_MEMBER
 (
-    ID BIGINT PRIMARY KEY,
+    USER_SEQ BIGINT PRIMARY KEY,
     USER_ID VARCHAR(255) NOT NULL,
     USER_NAME VARCHAR(255) NOT NULL,
-    USER_PED VARCHAR(255) NOT NULL
+    USER_PWD VARCHAR(255) NOT NULL
     );
 
 -- 보드 테이블 생성
-CREATE TABLE IF NOT EXISTS board_tb
+CREATE TABLE IF NOT EXISTS TB_BOARD
 (
-    board_Id BIGINT PRIMARY KEY,
+    board_seq  BIGINT PRIMARY KEY,
     board_Title VARCHAR(255) NOT NULL,
     board_content TEXT,
-    writer_Date TIMESTAMP NOT NULL,
+    write_Date TIMESTAMP NOT NULL,
     board_Count INT DEFAULT 0,
     member_Id BIGINT, -- 맴버 테이블의 ID를 참조하는 외래 키
     CONSTRAINT fk_member_id FOREIGN KEY (member_Id) REFERENCES member_tb (ID)
