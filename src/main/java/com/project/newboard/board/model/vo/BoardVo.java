f6de091aabea4ae1a292dc0a3af2880a9252e066
dasom2646 <choi.dasom2646@gmail.com>
2023-07-28 22:41:01 +0900

    데이터베이스 테이블을 전체적으로 수정 및 이름을 통일, 매퍼 설정 업데이트

--- src/main/java/com/project/newboard/board/model/vo/BoardVo.java
+++ src/main/java/com/project/newboard/board/model/vo/BoardVo.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.NotEmpty;
 @Data
 public class BoardVo {
 
-    private Long boardId; // 시퀀스
+    private Long boardSeq; // 게시판 시퀀스
     private String boardTitle; // 게시글 제목
     private String boardContent; // 게시글 내용
     private String writeDate; // 게시글 작성일
--- src/main/java/com/project/newboard/member/model/vo/MemberVo.java
+++ src/main/java/com/project/newboard/member/model/vo/MemberVo.java
@@ -7,15 +7,15 @@ import javax.validation.constraints.NotEmpty;
 
 @Data
 public class MemberVo {
-    private Long id; // 디비 관리되는아이디
+    private Long memberSeq; // 맴버 시퀀스
 
     @NotEmpty
-    private  String userId; // 로그인 ID
+    private  String memberId; // 로그인 ID
 
     @NotEmpty
-    private  String userName; // 사용자 이름
+    private  String memberName; // 사용자 이름
 
     @NotEmpty
-    private  String userPwd; // 사용자 비밀번호
+    private  String memberPwd; // 사용자 비밀번호
 
 }
