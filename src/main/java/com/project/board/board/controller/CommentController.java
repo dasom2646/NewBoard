99c6527c9a4607f210a90dc611e382674a400a20
dasom2646 <choi.dasom2646@gmail.com>
2023-08-23 22:04:08 +0900

    보드null값 해결

--- src/main/java/com/project/board/board/controller/CommentController.java
+++ src/main/java/com/project/board/board/controller/CommentController.java
@@ -7,6 +7,8 @@ import com.project.board.board.service.CommentService;
 import com.project.board.member.model.MemberDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,19 +28,22 @@ public class CommentController {
     /**
      * 댓글 등록 동작
      */
-    @PostMapping("/addComment")
-    public String addComment(@RequestParam Long boardSeq,
+    @PostMapping("/addComment/{boardSeq}")
+    public String addComment(@PathVariable Long boardSeq,
                              @RequestParam String commentContent,
                              HttpSession session ) {
         // 댓글 작성자 정보 가져오기
         MemberDto loggedInUser = (MemberDto) session.getAttribute("loggedInUser");
+
         // 해당 게시글 정보 가져오기
-        BoardDto boardDto = boardService.getBoardBySeq(boardSeq);
+        BoardDto board= boardService.getBoardBySeq(boardSeq);
+
+
         // CommentDto 생성 및 설정
         CommentDto commentDto = new CommentDto();
         commentDto.setContent(commentContent);
         commentDto.setMemberDto(loggedInUser);
-
+        commentDto.setBoardDto(board);
         // 댓글 저장
         commentService.addComment(boardSeq, commentDto);
 
